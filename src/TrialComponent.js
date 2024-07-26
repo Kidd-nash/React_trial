294e391c142d8da103405523c3b49b2185167566
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-26 13:25:26 +0800

    004 - button and onclick method to console log

--- src/TrialComponent.js
+++ src/TrialComponent.js
@@ -2,10 +2,15 @@ function TrialComponent() {
 
     let aVariable = "A variable string";
 
+    function clickMessage()  {
+        console.log("click to see message");
+    };
+
     return (
         <div>
             <p>String: {"A string"}</p>
             <p>Variable: {aVariable}</p>
+            <button onClick={clickMessage}>Click</button>
         </div>
     );
 }
