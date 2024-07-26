ddbeaae3452467a3ac2d4f460c4d7ab4ef25bbcb
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-26 13:20:48 +0800

    003 - displaying a variable

--- src/TrialComponent.js
+++ src/TrialComponent.js
@@ -1,7 +1,11 @@
 function TrialComponent() {
+
+    let aVariable = "A variable string";
+
     return (
         <div>
             <p>String: {"A string"}</p>
+            <p>Variable: {aVariable}</p>
         </div>
     );
 }
