d37c8012a1e74187570d7d03ce2d370475fb22f2
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-27 10:02:38 +0800

    010 - initializing CRUD

---
+++ src/crud.js
@@ -0,0 +1,22 @@
+class Crud {
+    //CRUD for reminders
+    //Create
+    create (task) {
+        console.log("creating");
+        console.log(task);
+    }
+    //Retrieve
+    retrieve () {
+        console.log("retrieving");
+    }
+    //Update
+    update (task) {
+        console.log("updating");
+        console.log(task);
+    }
+    //Delete
+    delete (task) {
+        console.log("deleting");
+        console.log(task);
+    }
+}
